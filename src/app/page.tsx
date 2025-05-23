0cdfd735154d02407a651d5c9ffc9962a392e276
Dan Billson <daniel.billson@paddle.com>
2025-05-23 16:19:44 +0100

    feat: add quote block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "@/components/hero";
+import { Quote } from "@/components/quote";
 import { Showcase } from "@/components/showcase";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Hero />
       <Showcase />
+      <Quote />
     </>
   );
 }
