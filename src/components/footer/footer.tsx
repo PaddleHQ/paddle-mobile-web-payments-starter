0e74fb06ca97f393e4c4b4456635886b5a55c737
Dan Billson <daniel.billson@paddle.com>
2025-05-29 16:20:43 +0100

    feat: add social icons

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,5 @@
 import { FooterBlur } from "@/components/footer/footer-blur";
+import { XIcon, LinkedInIcon, GithubIcon } from "@/components/footer/icons";
 import Link from "next/link";
 
 const links = [
@@ -57,15 +58,30 @@ const links = [
     title: "Follow Us",
     links: [
       {
-        label: "Twitter",
+        label: (
+          <div className="flex items-center gap-2">
+            <XIcon className="h-4 w-4" />
+            <span>Twitter</span>
+          </div>
+        ),
         href: "#",
       },
       {
-        label: "Linkedin",
+        label: (
+          <div className="flex items-center gap-2">
+            <LinkedInIcon className="h-4 w-4" />
+            <span>LinkedIn</span>
+          </div>
+        ),
         href: "#",
       },
       {
-        label: "Github",
+        label: (
+          <div className="flex items-center gap-2">
+            <GithubIcon className="h-4 w-4" />
+            <span>Github</span>
+          </div>
+        ),
         href: "#",
       },
     ],
@@ -80,9 +96,9 @@ export function Footer() {
         {links.map((link) => (
           <div key={link.title} className="mb-10 text-center">
             <h3 className="text-muted-foreground mb-8">{link.title}</h3>
-            <ul className="flex flex-col gap-8">
-              {link.links.map((link) => (
-                <li key={link.label}>
+            <ul className="flex flex-col items-center gap-8">
+              {link.links.map((link, index) => (
+                <li key={index}>
                   <Link href={link.href}>{link.label}</Link>
                 </li>
               ))}
