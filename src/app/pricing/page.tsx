d1da5cb08b69329e6b5f1a4652a523095fe101df
Dan Billson <daniel.billson@paddle.com>
2025-05-30 16:23:31 +0100

    fix: update nav position

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -4,8 +4,8 @@ import { RadialBlur } from "@/components/pricing/radial-blur";
 
 export default function Pricing() {
   return (
-    <div className="z-1 flex h-full min-h-screen w-full flex-col p-8">
-      <RadialBlur className="-top-40 md:-top-0" />
+    <div className="bg-card flex h-full min-h-screen w-full flex-col p-8">
+      <RadialBlur className="-top-40 z-0 md:-top-0" />
       <Nav />
       <PlanSelect />
     </div>
