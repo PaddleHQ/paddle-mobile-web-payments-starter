0a7f88b42630d7cb3c48e1df3c74877731dc88fe
Dan Billson <daniel.billson@paddle.com>
2025-06-10 12:55:23 +0100

    fix: update navigation links

---
+++ src/app/(legal)/privacy-policy/page.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "@/components/footer/footer";
+import { Nav } from "@/components/hero/nav";
+import { RadialBlur } from "@/components/pricing/radial-blur";
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="flex h-full min-h-screen w-full flex-col justify-between">
+      <div className="isolate flex w-full flex-col p-8">
+        <RadialBlur />
+        <Nav />
+        <h1 className="mt-20 text-center text-4xl font-medium tracking-tight sm:text-5xl">Privacy Policy</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
